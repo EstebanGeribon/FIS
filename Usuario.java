a09cf7fe6213319a730594c2bb4a939a3953fd10
EstebanGeribon <e.geribon@gmail.com>
2020-04-04 21:40:40 -0300

    B
    Se crea el Constructor, get & set.

--- Usuario.java
+++ Usuario.java
@@ -6,4 +6,56 @@ public class Usuario {
    int edad;
    int cantHijos;
    int saldo;
- 
+   
+   //Constructor
+   public Usuario(String nombre, int ced,String contrasena,int edad, int chijos,int saldo ){
+       nombre = "nombre";
+       ced = 1234567;
+       contrasena="pass";
+       edad=0;
+       chijos=0;
+       saldo=0;
+   }
+   //Get & Set
+   public String getNombre(){
+       return nombre;
+   }
+   public void setNombre(String nombre){
+       this.nombre=nombre;
+   }
+   public int getCedula(){
+       return cedula;
+   }
+   public void setCedula(int ci){
+       this.cedula = ci;           
+   }
+   public String getContrasena(){
+       return contarsena;
+   }
+   public void setContrasena(String contrasena){
+       this.contarsena = contrasena;
+   }
+   public int getEdad(){
+       return edad;
+   }
+   public void setEdad(int edad){
+       this.edad = edad;
+   }
+   public int getCantHijos(){
+       return cantHijos;
+   }
+   public void setCantHijos(int cant){
+       this.cantHijos = cant;
+   }
+   public int getSaldo(){
+       return saldo;
+   }
+   public void setSaldo(int saldo){
+       this.saldo = saldo;
+   }
+   
+   
+      
+}
+
+
